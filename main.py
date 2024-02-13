d52f93a3958c93901692889ec03b34dd5c84d0af
dimcodewritter <153201105+raphraphra@users.noreply.github.com>
2024-02-13 16:10:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,60 +1,13 @@
 from attributes.py import Attributes
 from parameters.py import Parameters
 
-def main() -> bool:
-    print("Hello. Let's a build a character !")
-    state_machine = "addAttribute"
-    parameters = Parameters()
+def create_attribute(name, [content]):
+    name = Attributes(name)
+    name.add_attr([content])
+
+def add_content(name, [content]):
+    name.add_attr([content])
+
+def remove_content(name, [content]):
     
-    match(state_machine):
-            
-        case "addAttribute":
-                
-            check = input("Build a new attribute? Yes or No.")
-                
-            while check == "Yes":
-                              
-                new_param = input("What attribute do you want to create?")
-                new_param = Attributes(str(new_param))
-                add = ""
-
-                while add != "/":
-
-                    add = input(f"Add an element into {new_param} (/ to escape).")
-
-                    new_param.add_attr(add)
-
-                check = input("Build another attribute?")
-
     
-
-        case "Delete":
-            check = "Yes"
-            while check == "Yes":
-                attr = input("Which attribute to delete from?")
-                parameters.remove_attr(attr)
-
-                check = input("Continue deleting?")
-
-
-        case "Display":
-            roll = "Yes"
-            while roll == "Yes":
-                parameters.__str__()
-                roll = input("Roll another time?")
-
-
-
-        case "Escape"
-    
-            return False
-
-        state_machine = input("What do you want to do?")
-
-
-while True:
-
-
-    
-
-
