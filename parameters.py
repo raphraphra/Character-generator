d2ba9a3c48adad8e5ebb01bf086ac7fb87548fb0
dimcodewritter <153201105+raphraphra@users.noreply.github.com>
2024-02-13 20:11:38 +0100

    Update parameters.py

--- parameters.py
+++ parameters.py
@@ -1,39 +1,37 @@
-from attributes.py import Attributes
+from attributes.py import Parameters
 
-class Parameters:
+class Container:
     
     def __init__(self, param = []): 
-        self.param = param
+        self.content = param
      
         
     def add_param(self, param):
-        for att in self.param:
+        for att in self.content:
             if att.name == str(param):  
-                self.param.remove(param)
-        self.param.append(param)
-        return self.param
+                self.content.remove(att)
+        self.content.append(param)
+        return self.content
         
-    def remove_attr(self, param):
+    def remove_attribute(self, param):
         cond = input("Remove an element or an entire parameter? E/P")
         if cond == "E": 
-            for index, att in enumerate(self.param):
+            for index, att in enumerate(self.content):
                 if att.name == param:
                     attrib = input("Which attribute to remove?")
-                    if attrib not in att.attribute:
-                        return f"{attrib} not found in {att}"
-                    att.attribute.remove(attrib)
-                    return att.attribute
+                    att.clear_attribute(attrib)
+                    
             return "Your demand was invalid"
             
         if cond == "P":
             for index, att in enumerate(self.param):
                 if att.name == param:
-                    self.param.pop(index)
+                    self.content.pop(index)
                     return self.param
             return f"{param} not found in parameters."
                   
     # Will interact better with python's builtin stuff
     def __str__(self):
-        res = [f"Your character's {att.name} is {att.pick()}" for att in self.param]
+        res = [f"Your character's {att.name} is {att.pick()}" for att in self.content]
         return "\n".join(res)
-
+    
