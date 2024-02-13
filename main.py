41202d5c7271ddbb78cc3c1a011a1e5d1e2cec56
IntScientist <153201105+raphraphra@users.noreply.github.com>
2024-02-13 15:35:58 +0100

    Added shitty interface

---
+++ main.py
@@ -0,0 +1,53 @@
+from attributes.py import Attributes
+from parameters.py import Parameters
+
+def main():
+    print("Hello. Let's a build a character !")
+    state_machine = "addAttribute"
+    parameters = Parameters([])
+    
+    match(state_machine):
+            
+        case "addAttribute":
+                
+            check = input("Build a new attribute? Yes or No.")
+                
+            while check == "Yes":
+                              
+                new_param = input("What attribute do you want to create?")
+                new_param = Attributes([], str(new_param))
+                add = ""
+
+                while add != "/":
+
+                    add = input(f"Add an element into {new_param} (/ to escape).")
+
+                    new_param.add_attr(add)
+
+                check = input("Build another attribute?")
+
+            state_machine = input("What do you want to do?")
+    
+
+        case "Delete":
+            check = "Yes"
+            while check == "Yes":
+                attr = input("Which attribute to delete from?")
+                parameters.remove_attr(attr)
+
+                check = input("Continue deleting?")
+
+            state_machine = input("What do you want to do?")
+            
+
+        case "Display":
+            roll = "Yes"
+            while roll == "Yes":
+                parameters.__str__()
+                roll = input("Roll another time?")
+            
+
+
+        case "Escape":
+
+            return 
