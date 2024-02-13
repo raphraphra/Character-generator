085632cc379fe8d27390eabbee5933e3626ed9b8
dimcodewritter <153201105+raphraphra@users.noreply.github.com>
2024-02-13 16:12:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from attributes.py import Attributes
 from parameters.py import Parameters
 
+
+def create_container(name, [content]):
 def create_attribute(name, [content]):
     name = Attributes(name)
     name.add_attr([content])
@@ -9,5 +11,7 @@ def add_content(name, [content]):
     name.add_attr([content])
 
 def remove_content(name, [content]):
+
+    
     
     
