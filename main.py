6421e51fa66f949fd4a9a46ced16c595d7411836
IntScientist <153201105+raphraphra@users.noreply.github.com>
2024-02-15 11:19:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,53 +1,76 @@
-from attributes.py import Attributes
 from parameters.py import Parameters
+from container.py import Container
+import sys
 
+dic = {}
+container = Container()
+seeds = []
+inp = ""
+    
+def create_parameter(*args):
+    dic[args[0]] = Parameters(args[0], [])
+    return  dic[args[0]].content
+
+def add_attribute(*args):
+    dic[args[0]].add_attr(args[1:])
+    return dic[args[0]].content
+
+def clear_attribute(*args):
+    dic[args[0]].clear_attribute(args[1:])
+    return dic[args[0]].content
+
+def add_parameter(*args):
+    container.add_param(dic[args[0]])
+    
+def display(*args):
+    for i in range(int(args[0])):
+        print(str(container))
+        seeds.append(str(container))
+    
+def parse_lign(string):
+    return string.split(" ")
+
+def stop():
+    print(seeds)
+    sys.exit()
+
+def show_commands():
+    for i in functions.keys()():
+        print(f"{i} \n")
+
+def show_param():
+    for i in dic.keys():
+        print(f"{i} \n")
+        
+def skip(*args):
+    print("Invalid Command")
+    pass
+
+def show_seeds(*args):
+    for i, j in enumerate(seeds):   
+        print(f"Seed {i} : {j} \n")
+
+functions = {
+    "c_param" : create_parameter,
+    "add_att": add_attribute,
+    "clear": clear_attribute,
+    "add_param": add_parameter,
+    "display" : display,
+    "stop": stop,
+    "show_commands" : show_commands,
+    "show_seeds" : show_seeds,
+    "show_param" : show_param
+}
+    
+    
 def main():
-    print("Hello. Let's a build a character !")
-    state_machine = "addAttribute"
-    parameters = Parameters([])
+    while True:
+        inp = parse_lign(input())
+        if inp == "display":
+            print(functions.get(inp[0], skadip)(*inp[1:]))
+            continue
     
-    match(state_machine):
-            
-        case "addAttribute":
-                
-            check = input("Build a new attribute? Yes or No.")
-                
-            while check == "Yes":
-                              
-                new_param = input("What attribute do you want to create?")
-                new_param = Attributes([], str(new_param))
-                add = ""
+        functions.get(inp[0], skip)(*inp[1:])
+        
+    return 
 
-                while add != "/":
-
-                    add = input(f"Add an element into {new_param} (/ to escape).")
-
-                    new_param.add_attr(add)
-
-                check = input("Build another attribute?")
-
-            state_machine = input("What do you want to do?")
-    
-
-        case "Delete":
-            check = "Yes"
-            while check == "Yes":
-                attr = input("Which attribute to delete from?")
-                parameters.remove_attr(attr)
-
-                check = input("Continue deleting?")
-
-            state_machine = input("What do you want to do?")
-            
-
-        case "Display":
-            roll = "Yes"
-            while roll == "Yes":
-                parameters.__str__()
-                roll = input("Roll another time?")
-            
-
-
-        case "Escape":
-
-            return 
