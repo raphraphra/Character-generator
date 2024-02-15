c8a6b687dd7397a9fb690234ee163d7417cd0b5f
dimcodewritter <153201105+raphraphra@users.noreply.github.com>
2024-02-15 16:20:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,70 +2,6 @@ from parameters.py import Parameters
 from container.py import Container
 import sys
 
-
-
-class Parameters:
-    
-    def __init__(self, name, attribute = []):
-        self.content = attribute
-        self.name = name 
-        
-    def clear_attribute(self,attribute):
-        self.content.remove(attribute)
-        return self.content
-        
-   
-    def add_attr(self, attribute):
-        for i in attribute:
-            if i in self.content:
-                self.content.remove(i)
-            self.content.append(i)
-        return self.content
-    
-    def pick(self):
-        return rd.choice(self.content)
-        
-       
-    
-class Container:
-    
-    def __init__(self, param = []): 
-        self.content = param
-     
-        
-    def add_param(self, param):
-        for att in self.content:
-            if att.name == str(param):  
-                self.content.remove(att)
-        self.content.append(param)
-        
-    def remove_attribute(self, param):
-        cond = input("Remove an element or an entire parameter? E/P")
-        if cond == "E": 
-            for index, att in enumerate(self.content):
-                if att.name == param:
-                    attrib = input("Which attribute to remove?")
-                    att.clear_attribute(attrib)
-                    
-            return "Your demand was invalid"
-            
-        if cond == "P":
-            for index, att in enumerate(self.param):
-                if att.name == param:
-                    self.content.pop(index)
-                    return self.param
-            return f"{param} not found in parameters."
-                  
-    # Will interact better with python's builtin stuff
-    def __str__(self):
-        res = [f"Your character's {att.name} is {att.pick()}" for att in self.content]
-        return "\n".join(res)
-    
-    
-
-
-    
-    
 dic = {}
 container = Container()
 seeds = []
@@ -88,11 +24,8 @@ def add_parameter(*args):
     
 def display(*args):
     for i in range(int(args[0])):
-        print(str(container))
+        print(str(container), end = "\n")
         seeds.append(str(container))
-    
-def parse_lign(string):
-    return string.split(" ")
 
 def stop():
     print(seeds)
@@ -130,7 +63,7 @@ functions = {
 def main():
     while True:
         inp = ""
-        inp = parse_lign(input())
+        inp = input().split(" ")
         if inp == "display":
             print(functions.get(inp[0], skip)(*inp[1:]))
             continue
